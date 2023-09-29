17d73bc81a3e126f7aabced78443c0c6b7d9c82f
Teelo <49792789+teelolws@users.noreply.github.com>
2023-09-29 20:37:14 +1300

    Override if Dragonriding is usable

--- Source/AutoBroomMount.lua
+++ Source/AutoBroomMount.lua
@@ -1,11 +1,14 @@
 local addonName, addon = ...
 
-EventUtil.RegisterOnceFrameEventAndCallback("PLAYER_ENTERING_WORLD", function()
-    local spellName = GetSpellInfo(419345)
-    local usable = IsUsableSpell(spellName)
+local function update()
+    local spellName = GetSpellInfo(368896)
+    local dragonridingUsable = IsUsableSpell(spellName)
+    
+    spellName = GetSpellInfo(419345)
+    local broomUsable = true--IsUsableSpell(spellName)
     local source, replacement = "C_MountJournal%.SummonByID%(0%)", "C_MountJournal.SummonByID(1799)"
     
-    if not usable then
+    if dragonridingUsable or (not broomUsable) then
         source, replacement = "C_MountJournal%.SummonByID%(1799%)", "C_MountJournal.SummonByID(0)"
     end
     
@@ -16,4 +19,7 @@ EventUtil.RegisterOnceFrameEventAndCallback("PLAYER_ENTERING_WORLD", function()
             EditMacro(macroIndex, name, icon, body)
         end
     end
-end)
+end
+
+EventUtil.RegisterOnceFrameEventAndCallback("PLAYER_ENTERING_WORLD", update)
+EventRegistry:RegisterFrameEventAndCallback("MOUNT_JOURNAL_USABILITY_CHANGED", update)
