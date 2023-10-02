020b0f44c6f007bf8121f3377eef5b36afecb83a
Teelo <49792789+teelolws@users.noreply.github.com>
2023-10-02 19:34:29 +1300

    Update AutoBroomMount.lua

--- Source/AutoBroomMount.lua
+++ Source/AutoBroomMount.lua
@@ -1,19 +1,30 @@
 local addonName, addon = ...
 
+local function unmute()
+    UnmuteSoundFile(567489)
+    UnmuteSoundFile(567524)
+end
+
 local function update()
+    if InCombatLockdown() then return end
+    
     local spellName = GetSpellInfo(368896)
     local dragonridingUsable = IsUsableSpell(spellName)
     
-    local broomUsable = false
+    local holidayActive = false
     for i = 1, GetNumRandomDungeons() do
         if GetLFGRandomDungeonInfo(i) == 285 then
-            broomUsable = true
+            holidayActive = true
         end
     end
     
+    local broomUsable = false
+    spellName = GetSpellInfo(419345)
+    broomUsable = IsUsableSpell(spellName)
+    
     local source, replacement = "C_MountJournal%.SummonByID%(0%)", "C_MountJournal.SummonByID(1799)"
     
-    if dragonridingUsable or (not broomUsable) then
+    if dragonridingUsable or (not holidayActive) or (not broomUsable) then
         source, replacement = "C_MountJournal%.SummonByID%(1799%)", "C_MountJournal.SummonByID(0)"
     end
     
@@ -22,6 +33,40 @@ local function update()
         if name and body and body:find(source) then
             body = body:gsub(source, replacement)
             EditMacro(macroIndex, name, icon, body)
+        end
+    end
+    
+    if GetCursorInfo() then return end
+    
+    if dragonridingUsable or (not holidayActive) or (not broomUsable) then
+        for slotID = 1, 120 do
+            if GetActionTexture(slotID) then
+                local actionType, actionID, subType = GetActionInfo(slotID)
+                if ((actionType == "summonmount") and (actionID == 1799)) or ((actionType == "companion") and (actionID == 419345) and (subType == "MOUNT")) then
+                    MuteSoundFile(567489)
+                    MuteSoundFile(567524)
+                    PickupAction(slotID)
+                    ClearCursor()
+                    C_MountJournal.Pickup(0)
+                    PlaceAction(slotID)
+                    RunNextFrame(unmute)
+                end
+            end
+        end
+    else
+        for slotID = 1, 120 do
+            if GetActionTexture(slotID) then
+                local actionType, actionID, subType = GetActionInfo(slotID)
+                if (actionType == "summonmount") and (actionID == 268435455) then
+                    MuteSoundFile(567489)
+                    MuteSoundFile(567524)
+                    PickupAction(slotID)
+                    ClearCursor()
+                    PickupSpell(419345)
+                    PlaceAction(slotID)
+                    RunNextFrame(unmute)
+                end
+            end
         end
     end
 end
